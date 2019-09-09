559f059e0a2b23e51692e59058b1b731da643126
Kyle Lin <kylejlin.us@gmail.com>
2019-09-08 17:58:42 -0700

    Check all divisions when determining if athlete is deletable
    ...in case somebody changes an athlete's division mid-season.

--- src/firestore/isAthleteDeletable.ts
+++ src/firestore/isAthleteDeletable.ts
@@ -1,6 +1,6 @@
 import firebase from "../firebase";
 import { Athlete } from "../types/misc";
-import { RaceDivisionUtil, RaceAction, RaceActionKind } from "../types/race";
+import { RaceAction, RaceActionKind } from "../types/race";
 
 const db = firebase.firestore();
 
@@ -34,32 +34,45 @@ function didAthleteFinishAtMeet(
   athlete: Athlete,
   meet: firebase.firestore.DocumentReference
 ): Promise<boolean> {
-  const divisionId = RaceDivisionUtil.stringify(
-    RaceDivisionUtil.getAthleteDivision(athlete)
-  );
   return meet
     .collection("races")
-    .doc(divisionId)
     .get()
-    .then(division =>
-      division.exists
-        ? division.ref
-            .collection("actionLists")
-            .doc(athlete.id)
-            .get()
-            .then(athleteActionListDoc => {
-              const data = athleteActionListDoc.data();
-              if (data === undefined) {
-                return false;
-              } else {
-                const mostRecentAction = data.actions[
-                  data.actions.length - 1
-                ] as RaceAction;
-                return mostRecentAction.kind !== RaceActionKind.Delete;
-              }
-            })
-        : false
+    .then(raceCollection =>
+      didAthleteFinishInAnyRace(
+        athlete,
+        raceCollection.docs.map(doc => doc.ref)
+      )
     );
+}
+
+function didAthleteFinishInAnyRace(
+  athlete: Athlete,
+  races: firebase.firestore.DocumentReference[]
+): Promise<boolean> {
+  return Promise.all(
+    races.map(race => didAthleteFinishInRace(athlete, race))
+  ).then(areAnyTrue);
+}
+
+function didAthleteFinishInRace(
+  athlete: Athlete,
+  race: firebase.firestore.DocumentReference
+): Promise<boolean> {
+  return race
+    .collection("actionLists")
+    .doc(athlete.id)
+    .get()
+    .then(athleteActionList => {
+      const data = athleteActionList.data();
+      if (data === undefined) {
+        return false;
+      } else {
+        const mostRecentAction = data.actions[
+          data.actions.length - 1
+        ] as RaceAction;
+        return mostRecentAction.kind !== RaceActionKind.Delete;
+      }
+    });
 }
 
 function areAnyTrue(arr: boolean[]): boolean {
