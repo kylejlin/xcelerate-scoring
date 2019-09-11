e99ca2173ce410850721f2b9db5aae5ec8c3bc9d
Kyle Lin <kylejlin.us@gmail.com>
2019-09-10 21:25:12 -0700

    Make firestore/addAthletesToSeason immediately resolve if empty array passed

--- src/firestore/addAthletesToSeason.ts
+++ src/firestore/addAthletesToSeason.ts
@@ -7,52 +7,58 @@ export default function addAthletesToSeason(
   athleteRows: HypotheticalAthlete[],
   seasonId: string
 ): Promise<void> {
-  const seasonRef = db.collection("seasons").doc(seasonId);
-  return db.runTransaction(transaction => {
-    return transaction.get(seasonRef).then(seasonDoc => {
-      const seasonData = seasonDoc.data();
-      if (seasonData === undefined) {
-        throw new Error(
-          "Attempted to addAthletesToSeason on nonexistent season."
-        );
-      } else {
-        const { athleteIdCounter } = seasonData;
-        if ("number" === typeof athleteIdCounter) {
-          const athletes: Athlete[] = athleteRows.map((row, i) => ({
-            ...row,
-            grade: row.grade.expect(
-              "Attempted to addAthletesToSeason when athlete " +
-                i +
-                " did not have a grade."
-            ),
-            gender: row.gender.expect(
-              "Attempted to addAthletesToSeason when athlete " +
-                i +
-                " did not have a gender."
-            ),
-            school: row.school.expect(
-              "Attempted to addAthletesToSeason when athlete " +
-                i +
-                " did not have a school."
-            ),
-            id: zeroPadToFiveDigits(athleteIdCounter + i),
-          }));
-          const newAthleteIdCounter = athleteIdCounter + athleteRows.length;
-          transaction.update(seasonRef, {
-            athleteIdCounter: newAthleteIdCounter,
-          });
-          athletes.forEach(athlete => {
-            const athleteRef = seasonRef.collection("athletes").doc(athlete.id);
-            transaction.set(athleteRef, athlete);
-          });
-        } else {
+  if (athleteRows.length > 0) {
+    const seasonRef = db.collection("seasons").doc(seasonId);
+    return db.runTransaction(transaction => {
+      return transaction.get(seasonRef).then(seasonDoc => {
+        const seasonData = seasonDoc.data();
+        if (seasonData === undefined) {
           throw new Error(
-            "Invalid season document: athleteIdCounter is not a number."
+            "Attempted to addAthletesToSeason on nonexistent season."
           );
+        } else {
+          const { athleteIdCounter } = seasonData;
+          if ("number" === typeof athleteIdCounter) {
+            const athletes: Athlete[] = athleteRows.map((row, i) => ({
+              ...row,
+              grade: row.grade.expect(
+                "Attempted to addAthletesToSeason when athlete " +
+                  i +
+                  " did not have a grade."
+              ),
+              gender: row.gender.expect(
+                "Attempted to addAthletesToSeason when athlete " +
+                  i +
+                  " did not have a gender."
+              ),
+              school: row.school.expect(
+                "Attempted to addAthletesToSeason when athlete " +
+                  i +
+                  " did not have a school."
+              ),
+              id: zeroPadToFiveDigits(athleteIdCounter + i),
+            }));
+            const newAthleteIdCounter = athleteIdCounter + athleteRows.length;
+            transaction.update(seasonRef, {
+              athleteIdCounter: newAthleteIdCounter,
+            });
+            athletes.forEach(athlete => {
+              const athleteRef = seasonRef
+                .collection("athletes")
+                .doc(athlete.id);
+              transaction.set(athleteRef, athlete);
+            });
+          } else {
+            throw new Error(
+              "Invalid season document: athleteIdCounter is not a number."
+            );
+          }
         }
-      }
+      });
     });
-  });
+  } else {
+    return Promise.resolve();
+  }
 }
 
 function zeroPadToFiveDigits(number: number): string {
